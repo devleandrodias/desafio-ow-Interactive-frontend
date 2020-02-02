371be0782839930c463640b3d1e4be38515c5707
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-02-02 08:55:41 -0300

    inicio do componente de modal

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,12 +1,17 @@
 <template>
-  <form-checkout />
+  <div class>
+    <form-checkout />
+    <modal-success />
+  </div>
 </template>
 
 <script>
 import FormCheckout from "../components/forms/FormCheckout";
+import ModalSuccess from "../components/widgets/ModalSuccess";
 export default {
   components: {
-    formCheckout: FormCheckout
+    formCheckout: FormCheckout,
+    modalSuccess: ModalSuccess
   }
 };
 </script>
